1074a2de94da7aa831af7e574d5013870b199a2c
zaynaba10 <r0614551@student.thomasmore.be>
2024-08-06 01:10:50 +0200

    update(login): update login page

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
-require 'config/config.php'; 
+require 'config/config.php';
 require 'models/User.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+
     try {
-        User::login($pdo, $username, $password);
-        
+        $user_id = User::login($pdo, $username, $password);
+        $_SESSION['user_id'] = $user_id;
+        echo 'User ID set in session: ' . $_SESSION['user_id'];
         header('Location: app.php');
         exit();
     } catch (Exception $e) {
         $_SESSION['error'] = $e->getMessage();
-        debug_to_console("error");
         header('Location: index.php');
         exit();
     }
